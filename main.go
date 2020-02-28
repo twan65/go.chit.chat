7fcfb9c43813a7acffcc09b83d664f12c1e6e0a4
twan65 <cx.twan65@gmail.com>
2020-02-28 22:56:32 +0900

    [add] main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static", http.StripPrefix("/static/", files))
+
+	mux.HandleFunc("/", func(arg1 http.ResponseWriter, arg2 *http.Request) {
+
+	})
+
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
+}
