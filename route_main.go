aa1d87c4b1153029a11ad4baf6dc02d729d55b38
twan65 <cx.twan65@gmail.com>
2020-02-29 21:45:06 +0900

    [add] generateHTML and index update

--- route_main.go
+++ route_main.go
@@ -1,32 +1,19 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
+func index(writer http.ResponseWriter, request *http.Request) {
 	threads, err := data.Threads()
-	if err == nil {
-		_, err := session(w, r)
-
-		public_tmpl_files := []string{
-			"templates/layout.html",
-			"templates/public.navbar.html",
-			"templates/index.html",
-		}
-
-		private_tmpl_files := []string{
-			"templates/layout.html",
-			"templates/private.navbar.html",
-			"templates/index.html",
-		}
-		var templates *template.Template
+	if err != nil {
+		error_message(writer, request, "Cannot get threads")
+	} else {
+		_, err := session(writer, request)
 		if err != nil {
-			templates := template.Must(template.ParseFiles(private_tmpl_files...))
+			generateHTML(writer, threads, "layout", "public.navbar", "index")
 		} else {
-			templates := template.Must(template.ParseFiles(public_tmpl_files...))
+			generateHTML(writer, threads, "layout", "private.navbar", "index")
 		}
-		templates.ExcuteTemplate(w, "layout", threads)
 	}
 }
--- utils.go
+++ utils.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"html/template"
 	"net/http"
 )
+
+func generateHTML(writer http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(writer, "layout", data)
+}
 
 func session(w http.ResponseWriter, r *http.Request) (sess data.Session, err error) {
 	// リクエストのクッキーを取り出す
